bdf8bcbe29c35bb35bccf22a87257478f218caa5
GrandScalleywag <paulinojayden9@gmail.com>
2025-08-06 21:28:36 +0000

    testimonial

--- script.js
+++ script.js
@@ -1,73 +1,68 @@
-const review = [
-    {
+const reviews = [{
         id: 1,
-        name: 'Susan Smith',
+        name: "Jayden Paulino",
         img: '#',
-        text: 'uhydfigdbh ugtdjnfbhb jdfhu erijkdf khurnj',
+        text: "Excellent service and friendly staff!",
+        date: "2023-10-01"
     },
-
-     {
+    {
         id: 2,
-        name: 'John Pork',
+        name: "Jane Smith",
         img: '#',
-        text: 'uhydfigdbh ugtdjnfbhb jdfhu erijkdf khurnj',
+        text: "Very good experience, will come again.",
+        date: "2023-10-02"          
     },
+  {
+        id: 1,
+        name: "nohj Doe",
+        img: '#',
+        text: " friendly staff!",
+        date: "2023-10-01"
+    },
+   
 
-     {
-        id: 3,
-        name: 'Peter Jones',
-        img: '#',
-        text: 'uhydfigdbh ugtdjnfbhb jdfhu erijkdf khurnj',
-    },
+
 ];
 
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
-const job = document.getElementById('job');
 const info = document.getElementById('info');
+const date = document.getElementById('date');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
 let currentItem = 0;
-window.addEventListener('DOMContentLoaded', function () {
-    showPerson();
-});
 
-function showPerson() {
-    const item = review[currentItem];
+window.addEventListener('DOMContentLoaded', function() {
+    const item = reviews[currentItem];
     img.src = item.img;
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
-};
+   
+});
 
-function showPerson(person){
+function showPerson(person) {
     const item = reviews[person];
     img.src = item.img;
     author.textContent = item.name;
-    job.textContent = item.job;
     info.textContent = item.text;
-};
+    date.textContent = item.date;
+}
 
-nextBtn.addEventListener(
-    'click',
-    function () {
-        currentItem++;
-        if (currentItem > review.length - 1) {
-            currentItem = 0;
-        }
-        showPerson(currentItem);
+nextBtn.addEventListener('click', function() {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
     }
-);
+    showPerson(currentItem);
+});
 
-prevBtn.addEventListener(
-    'click',
-    function () {
-        currentItem--;
-        if (currentItem < 0) {
-            currentItem = review.length - 1;
-        }
-        showPerson(currentItem);
+prevBtn.addEventListener('click', function() {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
     }
-);
+    showPerson(currentItem);
+});
